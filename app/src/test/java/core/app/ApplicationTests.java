f53bcbd6a993a82039e08fc285eb4ac0993f76a2
jbw9964 <jbw9964@gmail.com>
2024-10-26 12:09:57 +0900

    build: init module

---
+++ app/src/test/java/core/app/ApplicationTests.java
@@ -0,0 +1,14 @@
+package core.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+        // sample test
+    }
+
+}
